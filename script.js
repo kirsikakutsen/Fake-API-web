6cc0e2cdc3acf193e309438fea82e41c5aeb5e4c
kirsi <kirsika.kytsen@gmail.com>
2024-09-06 16:15:25 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => {
+        const template = document.querySelector('#comment-template').innerHTML;
+        data.forEach(user => {
+            const markup = template
+                .replace('{{name}}', user.name)
+                .replace('{{email}}', user.email)
+                .replace('{{body}}', user.body);
+
+            document.querySelector('div').insertAdjacentHTML('beforeend', markup);
+        });
+    })
+    .catch(error => console.log(error));ß
